0b8cdbbe4fd0cbbe9e0b54fa134ec005f8df841d
Beatrice Egumandi <106768860+icyybee@users.noreply.github.com>
2024-10-15 09:47:06 +0100

    test: complete cypress user journey

--- cypress/e2e/course.cy.ts
+++ cypress/e2e/course.cy.ts
@@ -2,11 +2,26 @@ describe("User flow to complete a course", () => {
   it("the user begins a course", () => {
     cy.visit("http://localhost:3000")
     cy.getByData("course-0").find("a").contains("Get started").click()
-    cy.location("pathname").should("equal", '/testing-your-first-application')
-    cy.getByData("next-lesson-button").contains('Start Course').click()
-    cy.location("pathname").should("equal", '/testing-your-first-application/app-install-and-overview')
+    cy.location("pathname").should("equal", "/testing-your-first-application")
+    cy.getByData("next-lesson-button").contains("Start Course").click()
+    cy.location("pathname").should(
+      "equal",
+      "/testing-your-first-application/app-install-and-overview"
+    )
     cy.getByData("challenge-answer-0").click()
-    cy.getByData("next-lesson-button").contains('Next Lesson').click()
-    cy.location("pathname").should("equal", '/testing-your-first-application/installing-cypress-and-writing-our-first-test')
+    cy.getByData("next-lesson-button").contains("Next Lesson").click()
+    cy.location("pathname").should(
+      "equal",
+      "/testing-your-first-application/installing-cypress-and-writing-our-first-test"
+    )
+    cy.getByData("challenge-answer-0").click()
+    cy.getByData("next-lesson-button").contains("Next Lesson").click()
+    cy.location("pathname").should(
+      "equal",
+      "/testing-your-first-application/setting-up-data-before-each-test"
+    )
+    cy.getByData("challenge-answer-0").click()
+    cy.getByData("next-lesson-button").contains("Complete Course").click()
+    cy.location("pathname").should("equal", "/")
   })
 })
