1879a2d159680d578656b824ddd81b18182acbfa
inesgcalvo <inglez@gmail.com>
2024-10-15 10:32:09 +0200

    test: course part 2

--- cypress/e2e/course.cy.ts
+++ cypress/e2e/course.cy.ts
@@ -2,5 +2,11 @@ describe("User flow to complete a course", () => {
   it("the user begins a course", () => {
     cy.visit("http://localhost:3000")
     cy.getByData("course-0").find("a").contains("Get started").click()
+    cy.location("pathname").should("equal", '/testing-your-first-application')
+    cy.getByData("next-lesson-button").contains('Start Course').click()
+    cy.location("pathname").should("equal", '/testing-your-first-application/app-install-and-overview')
+    cy.getByData("challenge-answer-0").click()
+    cy.getByData("next-lesson-button").contains('Next Lesson').click()
+    cy.location("pathname").should("equal", '/testing-your-first-application/installing-cypress-and-writing-our-first-test')
   })
 })
